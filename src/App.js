e6e017f77397d187c4a9f07711b37a395d64edde
alicalimli <alicalimli76@gmail.com>
2022-06-08 10:12:28 +0300

    created pet component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-App = () => {
-  return React.createElement(
-    "div",
-    { className: "bruh" },
-    React.createElement("h1", {}, "sdgagasdgsad")
-  );
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h1", {}, props.animal),
+    React.createElement("h1", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", { className: "bruh" }, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
