467f82d3fb1524ec88ad71a3de281bb7c85fddfb
alicalimli <alicalimli76@gmail.com>
2022-06-08 10:06:10 +0300

    seperated App component in index.html

---
+++ src/App.js
@@ -0,0 +1,9 @@
+App = () => {
+  return React.createElement(
+    "div",
+    { className: "bruh" },
+    React.createElement("h1", {}, "sdgagasdgsad")
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
