4fd25a9da3b192672e17902bc5f8f90ab1b0fdfb
alicalimli <alicalimli76@gmail.com>
2022-06-09 09:30:08 +0300

    added breed and animal option in the form

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,20 +1,62 @@
 import { useState } from "react";
 
-const SearchParams = () => {
-  const [location, setLocation] = useState("");
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
+const SearchParams = () => {
+  const [location, updateLocation] = useState("");
+  const [animal, updateAnimal] = useState("");
+  const [breed, updateBreed] = useState("");
+  const breeds = [];
   return (
     <div className="search-params">
-      <form action="#">
+      <form>
         <label htmlFor="location">
           Location
           <input
-            type="text"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
             id="location"
-            placeholder="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => updateLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              updateAnimal(e.target.value);
+              updateBreed("");
+            }}
+            onBlur={(e) => {
+              updateAnimal(e.target.value);
+              updateBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => updateBreed(e.target.value)}
+            onBlur={(e) => updateBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
