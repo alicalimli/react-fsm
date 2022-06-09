79c2eef72d4a10a9a840fcd4d931424915206599
alicalimli <alicalimli76@gmail.com>
2022-06-09 11:22:42 +0300

    added an error when there's no pet found

--- src/Results.js
+++ src/Results.js
@@ -3,9 +3,13 @@ import Pet from "./Pet";
 const Results = ({ pets }) => {
   return (
     <div>
-      {pets.map((pet) => (
-        <Pet name={pet.name} breed={pet.breed} key={pet.id} />
-      ))}
+      {!pets.length ? (
+        <h1>pets not found!</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet name={pet.name} breed={pet.breed} key={pet.id} />
+        ))
+      )}
     </div>
   );
 };
