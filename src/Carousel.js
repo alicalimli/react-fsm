ededf06dd63908b9d03613bb235b0283fd220e4f
alicalimli <alicalimli76@gmail.com>
2022-06-10 14:01:03 +0300

    made images clickable

---
+++ src/Carousel.js
@@ -0,0 +1,45 @@
+import { Component } from "react";
+
+class Carousel extends Component {
+  state = {
+    active: 0,
+  };
+
+  static defaultProps = {
+    images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+  };
+
+  indexClicked = (e) => {
+    console.log(e.target.dataset.index);
+    e.target.classList.add("active");
+    this.setState({
+      active: +e.target.dataset.index,
+    });
+  };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+    console.log(images);
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              src={photo}
+              onClick={this.indexClicked}
+              data-index={index}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
