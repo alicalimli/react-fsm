63b048e4af7d968b5da6e531743052b88bbbc73a
alicalimli <alicalimli76@gmail.com>
2022-06-10 10:57:28 +0300

    imported details.js and react router dom components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
