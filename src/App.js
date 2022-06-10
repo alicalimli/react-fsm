f5fb6c3c3cdb127d8b78d6445a0e25611080a8ff
alicalimli <alicalimli76@gmail.com>
2022-06-10 11:10:05 +0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import { StrictMode } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
